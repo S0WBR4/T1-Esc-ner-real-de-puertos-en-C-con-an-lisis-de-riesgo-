f92800a91d8f46736f787de5d879c05ec9320cc2
S0WBR4 <133049076+S0WBR4@users.noreply.github.com>
2025-09-07 19:21:28 -0600

    Update Registro.h

--- Registro.h
+++ Registro.h
@@ -1,15 +1,9 @@
 // Responsable: Juan Angel Rodriguez Bulnes
 // Módulo: Genera el reporte del escaneo en un archivo de texto.
-// En este archivo solo se declara la función que se usará en el main.
-
 #ifndef REGISTRO_H
 #define REGISTRO_H
-
 #include <string>
 #include "Escaneo.h"
-
-// Esta función guarda todos los resultados del escaneo en un archivo.
-// Recibe la IP, la lista de puertos escaneados y el nivel de sensibilidad elegido.
 void guardarResultados(const std::string& archivo,
                        const std::string& ip,
                        const Puerto* resultados, int cantidad,
